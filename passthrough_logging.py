0a0ea23f1b7c0b5b8984fd58479a704069367302
Rick Mark <rickmark@dropbox.com>
2019-04-08 16:18:51 -0700

    Fixes

--- passthrough_logging.py
+++ passthrough_logging.py
@@ -13,8 +13,8 @@ from hashlib import sha256
 
 from fuse import FUSE, FuseOSError, Operations
 
-LOGFILE = None
-LOGDB = None
+LOG_FILE = None
+LOG_DB = None
 LOG_CALLS = set()
 LOG_ALL = False
 
@@ -47,62 +47,65 @@ TABLES = [
      
 
 LOGGERS = {
-    "call": ("INSERT INTO func_calls (call, args, kwargs, retval) VALUES (%s, %s, %s, %s, %s)", ('ts', 'call', 'args, 'kwargs', 'retval')),
-    "read": ("INSERT INTO reads (path, length, offset, buffer_length, buffer_hash) VALUES (%s, %s, %s, %s, %s)", ('path, 'length', 'offset', 'buffer_length', 'buffer_hash')),
-    "write": ("INSERT INTO writes (path, offset, buffer_length, buffer_hash) VALUES (%s, %s, %s, %s)" ('path', 'offset', 'buffer_length', 'buffer_hash'))
+    "call": ("INSERT INTO func_calls (call, args, kwargs, retval) VALUES (%s, %s, %s, %s, %s)", ('ts', 'call', 'args', 'kwargs', 'retval')),
+    "read": ("INSERT INTO reads (path, length, offset, buffer_length, buffer_hash) VALUES (%s, %s, %s, %s, %s)", ('path', 'length', 'offset', 'buffer_length', 'buffer_hash')),
+    "write": ("INSERT INTO writes (path, offset, buffer_length, buffer_hash) VALUES (%s, %s, %s, %s)", ('path', 'offset', 'buffer_length', 'buffer_hash'))
 }
 
+
 def log_init(logfnm, logdb):
-    global LOGFILE
-    global LOGDB
+    global LOG_FILE
+    global LOG_DB
     if logfnm:
         if not os.path.exists(logfnm):
-            LOGFILE = open(logfnm, 'w')
+            LOG_FILE = open(logfnm, 'w')
         else:
-            LOGFILE = open(logfnm, 'a')
+            LOG_FILE = open(logfnm, 'a')
 
     if logdb:
         conn = sqlite3.connect(logdb)
-        LOGDB = conn.cursor()
+        LOG_DB = conn.cursor()
         for table in TABLES:
-            LOGDB.execute(table)
+            LOG_DB.execute(table)
 
 
 def log(call, message):
-    if not call in LOGCALLS:
+    if call not in LOG_CALLS:
         return
-    if LOGFILE:
-        LOGFILE.write("%s: %s\n" % (call, message))
+    if LOG_FILE:
+        LOG_FILE.write("%s: %s\n" % (call, message))
+
 
 def logdb(call, info):
     def run_query():
-        if not call in LOGGERS:
+        if call not in LOGGERS:
             return
         query, args = LOGGERS.get(call)
-        LOGDB.execute(query, (info[i] for i in args))
+        LOG_DB.execute(query, (info[i] for i in args))
 
     info['call'] = call
     info['buffer_length'] = len(buf)
     info['buffer_hash'] = sha256(buf).hexdigest()
     if LOG_ALL:
         info['retval'] = info.get('buf')
-        LOGDB.execute(LOGGERS['call'], {k: str(v) for k, v in info.iteritems()})
+        LOG_DB.execute(LOGGERS['call'], {k: str(v) for k, v in info.iteritems()})
     run_query()
+
 
 def logwrapper(func):
     def wrapper(*args, **kwargs):
         info = dict(zip(func.__code__.co_varnames, args))
         info.update(kwargs)
-        log(call, "args, kwargs %s, %s" % (args, kwargs))
+        log(func.__name__, "args, kwargs %s, %s" % (args, kwargs))
         try:
             res = func(*args, **kwargs)
             info['buf'] = marshal.dumps(res)
-            log(call, "yielded %s" % (res))
-            logdb(call, info)
+            log(func.__name__, "yielded %s" % res)
+            logdb(func.__name__, info)
         except Exception as e:
-            log(call, "raised %s" % e)
-            info['buf']: str(e).encode('utf8')
-            logdb(call, info)
+            log(func.__name__, "raised %s" % e)
+            info['buf']: str[e].encode('utf8')
+            logdb(func.__name__, info)
         return res
     return wrapper
 
@@ -247,17 +250,18 @@ class Passthrough(Operations):
         return self.flush(path, fh)
 
 
-def main(mountpoint, root):
-    FUSE(Passthrough(root), mountpoint, nothreads=True, foreground=True)
+def main(mount_point, root):
+    FUSE(Passthrough(root), mount_point, nothreads=True, foreground=True)
+
 
 if __name__ == '__main__':
     import argparse
-    argp = argparse.ArgumentParser()
-    argp.add_argument('-m', '--mountpoint', help="Mountpoint to use")
-    argp.add_argument('-r', '--root', help="Root to mount at mountpoint")
-    argp.add_argument('-l', '--logfile', help="Logfile to use.")
-    argp.add_argument('-d', '--logdb', help="Log to an sqlite DB instead")
-    argp.add_argument('-a', '--logall', help="Log everything (DO NOT USE)")
-    argp.add_argument('-c', '--calllog', action='append', help="Use multiple to log only these calls (read, write, etc)")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mountpoint', help="Mountpoint to use")
+    parser.add_argument('-r', '--root', help="Root to mount at mountpoint")
+    parser.add_argument('-l', '--logfile', help="Logfile to use.")
+    parser.add_argument('-d', '--logdb', help="Log to an sqlite DB instead")
+    parser.add_argument('-a', '--logall', help="Log everything (DO NOT USE)")
+    parser.add_argument('-c', '--calllog', action='append', help="Use multiple to log only these calls (read, write, etc)")
 
     main(sys.argv[2], sys.argv[1])
